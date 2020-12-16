6dedca21ac5ffc2c2a58373007fd6673392a547d
Lucien <sim322on@yahoo.com.tw>
2020-12-17 00:17:40 +0800

    edit login.html

--- dist/assets/js/all.js
+++ dist/assets/js/all.js
@@ -1,7 +1,31 @@
 "use strict";
 
-console.log('Hello!');
 $(document).ready(function () {
-  console.log('HesSchool Hello!');
+  $('#loginButton').click(function (e) {
+    e.preventDefault();
+    $('#InputPassword').removeClass('is-invalid');
+    $('#InputEAccount').removeClass('is-invalid');
+
+    if ($('#InputEAccount').val() == '' || $('#InputPassword').val() == '') {
+      // alert('帳號密碼不得為空')
+      if ($('#InputEAccount').val() == '') {
+        $('#InputEAccount').addClass('is-invalid ');
+        $('#InputEAccount').focus();
+      } else {
+        $('#InputEAccount').addClass('is-valid ');
+      }
+
+      if ($('#InputPassword').val() == '') {
+        $('#InputPassword').addClass('is-invalid ');
+        $('#InputPassword').focus();
+      } else {
+        $('#InputPassword').addClass('is-valid');
+      }
+    } else {
+      window.location.href = 'https://dstsidragon.github.io/CssOnLineClass_SixMisson/dist/member.html';
+      $('#InputEAccount').val('');
+      $('#InputPassword').val('');
+    }
+  });
 });
 //# sourceMappingURL=all.js.map
