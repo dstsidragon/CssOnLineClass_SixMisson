5e763be1be4e8c79af98ca149a169ce2ac64b978
Lucien <sim322on@yahoo.com.tw>
2020-12-18 02:31:36 +0800

    add member changePassword js

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,7 +1,7 @@
 
 $(document).ready(() => {
   
-  
+  //登入頁面
   $('#loginButton').click(function (e) {
     e.preventDefault();
     $('#InputPassword').removeClass('is-invalid');
@@ -32,4 +32,54 @@ $(document).ready(() => {
     }
 
 });
+
+  //會員頁面
+  $('#changePasswordButton').click(function (e) {
+    e.preventDefault();
+    $('#OldPassword').removeClass('is-invalid');
+    $('#NewPassword').removeClass('is-invalid');
+    $('#checkNewPassword').removeClass('is-invalid');
+    $('#OldPassword').removeClass('is-valid');
+    $('#NewPassword').removeClass('is-valid');
+    $('#checkNewPassword').removeClass('is-valid');
+
+    if ($('#OldPassword').val() == '' || $('#NewPassword').val() == ''||$('#checkNewPassword').val() == '' ) {
+        // alert('帳號密碼不得為空')
+        if ($('#OldPassword').val() == ''){
+          $('#OldPassword').addClass('is-invalid ');
+          $('#OldPassword').focus();
+        }else{
+          
+          $('#OldPassword').addClass('is-valid ');
+        }
+        if ($('#NewPassword').val() == ''){
+          $('#NewPassword').addClass('is-invalid ');
+          $('#NewPassword').focus();
+        }else{
+          $('#NewPassword').addClass('is-valid');
+        }
+        if ($('#checkNewPassword').val() == ''){
+          $('#checkNewPassword').addClass('is-invalid ');
+          $('#checkNewPassword').focus();
+        }else{
+          $('#checkNewPassword').addClass('is-valid');
+        }
+
+        }else {
+      if ( $('#NewPassword').val() == $('#checkNewPassword').val() ) {
+        alert('密碼修改成功');
+        $('#OldPassword').val('');
+        $('#NewPassword').val('');
+        $('#checkNewPassword').val('');
+        
+      }else{
+        alert('新密碼兩次輸入不同!');
+        $('#NewPassword').val('');
+        $('#checkNewPassword').val('');
+         
+      }
+  }
+ 
+}
+)
 });
--- dist/assets/js/all.js
+++ dist/assets/js/all.js
@@ -1,6 +1,7 @@
 "use strict";
 
 $(document).ready(function () {
+  //登入頁面
   $('#loginButton').click(function (e) {
     e.preventDefault();
     $('#InputPassword').removeClass('is-invalid');
@@ -27,6 +28,51 @@ $(document).ready(function () {
       window.location.href = 'member.html';
       $('#InputEAccount').val('');
       $('#InputPassword').val('');
+    }
+  }); //會員頁面
+
+  $('#changePasswordButton').click(function (e) {
+    e.preventDefault();
+    $('#OldPassword').removeClass('is-invalid');
+    $('#NewPassword').removeClass('is-invalid');
+    $('#checkNewPassword').removeClass('is-invalid');
+    $('#OldPassword').removeClass('is-valid');
+    $('#NewPassword').removeClass('is-valid');
+    $('#checkNewPassword').removeClass('is-valid');
+
+    if ($('#OldPassword').val() == '' || $('#NewPassword').val() == '' || $('#checkNewPassword').val() == '') {
+      // alert('帳號密碼不得為空')
+      if ($('#OldPassword').val() == '') {
+        $('#OldPassword').addClass('is-invalid ');
+        $('#OldPassword').focus();
+      } else {
+        $('#OldPassword').addClass('is-valid ');
+      }
+
+      if ($('#NewPassword').val() == '') {
+        $('#NewPassword').addClass('is-invalid ');
+        $('#NewPassword').focus();
+      } else {
+        $('#NewPassword').addClass('is-valid');
+      }
+
+      if ($('#checkNewPassword').val() == '') {
+        $('#checkNewPassword').addClass('is-invalid ');
+        $('#checkNewPassword').focus();
+      } else {
+        $('#checkNewPassword').addClass('is-valid');
+      }
+    } else {
+      if ($('#NewPassword').val() == $('#checkNewPassword').val()) {
+        alert('密碼修改成功');
+        $('#OldPassword').val('');
+        $('#NewPassword').val('');
+        $('#checkNewPassword').val('');
+      } else {
+        alert('新密碼兩次輸入不同!');
+        $('#NewPassword').val('');
+        $('#checkNewPassword').val('');
+      }
     }
   });
 });
